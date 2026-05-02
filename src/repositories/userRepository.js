6e91c70b890d524fffeca8784602a39da212981b
dayeon <dayeon2423004@g.yju.ac.kr>
2026-05-02 23:07:59 +0900

    feat: 회원 탈퇴, 유저 조회, 닉네임 변경 리포지토리 구현

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,26 @@
+import prisma from "../lib/prisma";
+
+export const findUserById = async (id) => {
+    return prisma.user.findFirst({
+        where : { id, deletedAt : null }
+    })
+};
+
+export const deleteUserById = async (id) => {
+    return prisma.user.update({
+        where : { id, deletedAt : null },
+        data : {
+            deletedAt : new Date(),
+            email: `deleted_${id}_${Date.now()}`,
+            providerId: `deleted_${id}_${Date.now()}`,
+            nickname : null,
+        }
+    });
+};
+
+export const updateUserNickname = async (id, nickname) => {
+    return prisma.user.update({
+        where : { id, deletedAt : null },
+        data : { nickname }
+    });
+};
