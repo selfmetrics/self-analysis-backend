8cee21f1d961a35bfffed7c017e942ced3fdf775
dayeon <dayeon2423004@g.yju.ac.kr>
2026-05-04 00:02:18 +0900

    fix: 인증 미들웨어 보완

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,16 +1,25 @@
 import { verifyJWT } from "../utils/jwt.js";
 
-export default authMiddleware = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
 
+        // 헤더가 없는 경우
         if (!authHeader) {
             const err = new Error("토큰이 없습니다.");
             err.status = 401;
+            return next(err);
         }
 
         // Bearer Token 중 Token만 가져와 검증
         const token = authHeader.split(" ")[1];
+
+        // 토큰이 없는 경우
+        if (!token) {
+            const err = new Error("토큰이 없습니다.");
+            err.status = 401;
+            return next(err);
+        }
 
         const decoded = verifyJWT(token);
 
@@ -19,6 +28,10 @@ export default authMiddleware = (req, res, next) => {
 
         next();
     } catch (err) {
-        throw new Error("토큰 확인에 실패하였습니다.");
+        const error = new Error("토큰 확인에 실패하였습니다.");
+        error.status = 401;
+        next(error);
     }
 };
+
+export default authMiddleware;
