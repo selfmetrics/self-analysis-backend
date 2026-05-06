5a34c4ffe9ef8524b2c74901e79a65f00540099a
dayeon <163419727+dayeon2423004@users.noreply.github.com>
2026-05-06 16:04:22 +0900

    Feature/interview (#20)
    * fix: 면접 질문 삭제 시 답변 레코드 연관 삭제
    
    * feat: 면접 질문 관련 CRUD 로직 추가

--- src/controllers/interviewController.js
+++ src/controllers/interviewController.js
@@ -1,8 +1,11 @@
 import { createInterviewQuestionService, getInterviewQuestionsService, getInterviewQuestionByIdService, updateInterviewQuestionService, deleteInterviewQuestionService } from "../services/interviewService.js";
+import { created, noContent, success } from "../utils/responses.js";
 
 export const createQuestion = async(req, res, next) => {
     try {
+        const result = await createInterviewQuestionService(req.userId, req.body.question);
 
+        return created(res, result, "면접 질문 생성에 성공하였습니다.");
     } catch (err) {
         next(err);
     }
@@ -10,7 +13,8 @@ export const createQuestion = async(req, res, next) => {
 
 export const getQuestions = async(req, res, next) => {
     try {
-
+        const result = await getInterviewQuestionsService(req.userId);
+        return success(res, result, "면접 질문 조회에 성공하였습니다.");
     } catch (err) {
         next(err);
     }
@@ -18,7 +22,9 @@ export const getQuestions = async(req, res, next) => {
 
 export const getQuestionById = async(req, res, next) => {
     try {
-
+        const result = await getInterviewQuestionByIdService(req.userId, req.params.questionId);
+    
+        return success(res, result, "면접 상세 조회에 성공하였습니다.");
     } catch (err) {
         next(err);
     }
@@ -26,7 +32,9 @@ export const getQuestionById = async(req, res, next) => {
 
 export const updateQuestion = async(req, res, next) => {
     try {
+        const result = await updateInterviewQuestionService(req.userId, req.params.questionId, req.body.question, req.body.answer);
 
+        return success(res, result, "질문 답변 수정에 성공하였습니다.");
     } catch (err) {
         next(err);
     }
@@ -34,7 +42,9 @@ export const updateQuestion = async(req, res, next) => {
 
 export const deleteQuestion = async(req, res, next) => {
     try {
-
+        const result = await deleteInterviewQuestionService(req.userId, req.params.questionId);
+    
+        return noContent(res);
     } catch (err) {
         next(err);
     }
--- src/services/interviewService.js
+++ src/services/interviewService.js
@@ -1,21 +1,22 @@
+import { updateQuestionAnswer } from "../repositories/episodeRepository.js";
 import { createInterviewQuestion, findInterviewQuestions, findInterviewQuestionById, updateInterviewQuestion, deleteInterviewQuestionById } from "../repositories/interviewRepository.js";
 
-export const createInterviewQuestionService = async() => {
-
+export const createInterviewQuestionService = async(userId, question) => {
+    return createInterviewQuestion(userId, question);
 };
 
-export const getInterviewQuestionsService = async() => {
-
+export const getInterviewQuestionsService = async(userId) => {
+    return findInterviewQuestions(userId);
 };
 
-export const getInterviewQuestionByIdService = async() => {
-
+export const getInterviewQuestionByIdService = async(userId, questionId) => {
+    return findInterviewQuestionById(userId, questionId);
 };
 
-export const updateInterviewQuestionService = async() => {
-
+export const updateInterviewQuestionService = async(userId, questionId, question, answer) => {
+    return updateInterviewQuestion(userId, questionId, question, answer);
 };
 
-export const deleteInterviewQuestionService = async() => {
-
+export const deleteInterviewQuestionService = async(userId, questionId) => {
+    return deleteInterviewQuestionById(userId, questionId);
 };
