12776f785145961ef63da23b24b859556cfa558a
kimdayeon <dayeon2423004@g.yju.ac.kr>
2026-05-06 17:58:26 +0900

    feat : 감정 집계 조회 로직 구현

--- src/services/analyticsService.js
+++ src/services/analyticsService.js
@@ -1,5 +1,5 @@
 import { aggregateEmotionStats } from "../repositories/analyticsRepository.js";
 
-export const getEmotionAnalyticsService = async() => {
-
+export const getEmotionAnalyticsService = async(userId, startDate, endDate) => {
+    return aggregateEmotionStats(userId, startDate, endDate);
 };
